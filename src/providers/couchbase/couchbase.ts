9ee7f978e73078122379f8354d0f080fdce37176
Thanh Pham <thanh.pham@innovexdownhole.com>
2018-05-31 14:01:24 -0500

    provider

---
+++ src/providers/couchbase/couchbase.ts
@@ -0,0 +1,49 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Http } from '@angular/http';
+import { Platform } from 'ionic-angular';
+import { Couchbase, Database } from "cordova-couchbase/core";
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class CouchbaseProvider {
+
+  private isInstantiated: boolean;
+  private database: Database;
+  private listener: EventEmitter<any> = new EventEmitter();
+
+  public constructor(public http: Http, platform: Platform) {
+      if(!this.isInstantiated) {
+          platform.ready().then(() => {
+              (new Couchbase()).openDatabase("nraboy").then(database => {
+                  this.database = database;
+                  let views = {
+                    items: {
+                        map: function(doc) {
+                            if(doc.type == "list" && doc.title) {
+                                this.listener.emit(doc._id, {title: doc.title, rev: doc._rev})
+                            }
+                        }.toString()
+                    }
+                };
+                this.database.createDesignDocument("_design/todo", views);
+                this.database.listen(change => {
+                    this.listener.emit(change.detail);
+                });
+                  this.isInstantiated = true;
+              }, error => {
+                  console.error(error);
+              });
+          });
+      }
+  }
+
+  public getDatabase() {
+    return this.database;
+  }
+
+  public getChangeListener(): EventEmitter<any> {
+    return this.listener;
+}
+
+
+}
